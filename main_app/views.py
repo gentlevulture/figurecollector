7d13885149fbc9cb2ade82900c8c310cb3d8015a
gentlevulture <novemberboughs@gmail.com>
2021-11-22 17:43:16 -0800

    htmlTemplateSetup

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
   return HttpResponse('<h1>Hello Action Figure Collector</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the Action Figure Collector</h1>')
+  return render(request, 'about.html')

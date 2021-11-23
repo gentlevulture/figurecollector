75e8904d6b4dda2096466072bfd9c3075b7e8334
gentlevulture <novemberboughs@gmail.com>
2021-11-22 19:49:34 -0800

    renderDataTemplate

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class Figure:
+  def __init__(self, name, brand, description, scale):
+    self.name = name
+    self.brand = brand
+    self.description = description
+    self.scale = scale
+
+figures = [
+  Figure('Medieval Spawn III', 'McFarlane', 'Missing shield.', 6),
+  Figure('Grand Admiral Thrawn', 'Hasbro', 'On card.', 6),
+  Figure('Boba Fett', 'Hasbro', 'On card.', 3.75),
+  Figure('Batman - DC Artists Alley', 'DC Entertainment', 'Designed by nooligan. Loose.', 6)
+]
 def home(request):
   return HttpResponse('<h1>Hello Action Figure Collector</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the Action Figure Collector</h1>')
+  return render(request, 'about.html')
+
+def figures_index(request):
+  return render(request, 'figures/index.html', { 'figures': figures })
